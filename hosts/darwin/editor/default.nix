3ee7f340e67f2ac08632c1ac2d5930bd5501570f
Lain Iwakura <lain@iwakura.page>
2026-06-22 17:24:39 +0300

    feat: zed!!

--- hosts/darwin/editor/default.nix
+++ hosts/darwin/editor/default.nix
@@ -4,5 +4,6 @@
   imports = [
     ./nvim.nix
     ./codeum.nix
+    ./zed.nix
   ];
 }
---
+++ hosts/darwin/editor/zed.nix
@@ -0,0 +1,36 @@
+{ ... }:
+{
+  programs.zed-editor = {
+    enable = true;
+
+    extensions = [
+      "catppuccin"
+      "nix"
+      "proto"
+      "toml"
+      "java"
+      "csharp"
+      "terraform"
+      "hcl"
+      "bazel"
+      "ocaml"
+      "erlang"
+      "astro"
+      "mdx"
+      "wakatime"
+    ];
+
+    userSettings = {
+      theme = "Catppuccin Mocha";
+      disable_ai = true;
+      vim_mode = true;
+      auto_update = false;
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      buffer_font_family = "Iosevka";
+      ui_font_family = "Iosevka";
+    };
+  };
+}
