d4afad98ec09fbf08793fc3df8fa3c23b65603a6
Lain Iwakura <lain@iwakura.page>
2026-06-22 17:43:31 +0300

    fix: some extensions fix and tls

--- hosts/darwin/editor/zed.nix
+++ hosts/darwin/editor/zed.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zed-editor = {
     enable = true;
@@ -12,21 +12,22 @@
       "csharp"
       "terraform"
       "hcl"
-      "bazel"
+      "starlark"
       "ocaml"
       "erlang"
       "astro"
       "mdx"
       "wakatime"
+      "dockerfile"
     ];
 
     userSettings = {
       theme = "Catppuccin Mocha";
       disable_ai = true;
+      vim_mode = false;
       project_panel = {
         dock = "left";
       };
-      vim_mode = true;
       auto_update = false;
       telemetry = {
         diagnostics = false;
@@ -34,6 +35,32 @@
       };
       buffer_font_family = "Iosevka";
       ui_font_family = "Iosevka";
+      lsp = {
+        # pin starpls from nixpkgs instead of letting the bazel extension
+        # pull a binary from github releases at runtime. starpls shells out to
+        # a binary literally named `bazel` for `bazel info`, but we only have
+        # bazelisk, so point it there explicitly via --bazel_path.
+        starpls.binary = {
+          path = "${pkgs.starpls}/bin/starpls";
+          arguments = [
+            "server"
+            "--bazel_path=${pkgs.bazelisk}/bin/bazelisk"
+          ];
+        };
+      };
+      languages = {
+        Starlark = {
+          formatter = {
+            external = {
+              command = "${pkgs.bazel-buildtools}/bin/buildifier";
+              arguments = [
+                "-path"
+                "{buffer_path}"
+              ];
+            };
+          };
+        };
+      };
     };
   };
 }
