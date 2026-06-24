aa098f29049c0f2f18d152b9498fc02372baf9d5
Lain Iwakura <lain@iwakura.page>
2026-06-24 22:18:47 +0300

    chore: sync Wed Jun 24 22:18:47 MSK 2026

--- hosts/macmini/services/forgejo/default.nix
+++ hosts/macmini/services/forgejo/default.nix
@@ -58,7 +58,7 @@
         ENABLE_OPENID_SIGNUP = false;
       };
       actions = {
-        ENABLED = false;
+        ENABLED = true; #SOMTHING HAPPENS
       };
       other = {
         SHOW_FOOTER_VERSION = false;
