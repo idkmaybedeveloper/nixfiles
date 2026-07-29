4b1115a172c95bcc4cec163042fe7c8abd793cdb
Lain Iwakura <lain@iwakura.page>
2026-07-29 23:52:43 +0300

    feat: vivienne for two scrobblers

--- hosts/darwin/services/vivienne.nix
+++ hosts/darwin/services/vivienne.nix
@@ -6,6 +6,18 @@
   ...
 }:
 
+let
+  tomlFormat = pkgs.formats.toml { };
+  mkHome = "/Users/lain/.local/state/vivienne-mk";
+
+  mkConfig = tomlFormat.generate "vivienne-mk-config.toml" {
+    server = "https://fm.pooziqo.fun";
+    username = "lain";
+    password_file = config.sops.secrets.vivienne_mk_password.path;
+    api_key = "vivienne";
+    shared_secret = "";
+  };
+in
 {
   sops = {
     defaultSopsFile = abs "secrets/m68k.yaml";
@@ -17,6 +29,9 @@
     secrets.vivienne_password = {
       owner = "lain";
     };
+    secrets.vivienne_mk_password = {
+      owner = "lain";
+    };
   };
 
   services.vivienne = {
@@ -24,6 +39,20 @@
     config = {
       username_file = config.sops.secrets.vivienne_username.path;
       password_file = config.sops.secrets.vivienne_password.path;
+    };
+  };
+
+  launchd.user.agents.vivienne-mk = {
+    serviceConfig = {
+      ProgramArguments = [ "${config.services.vivienne.package}/bin/vivienne" ];
+      EnvironmentVariables = {
+        VIVIENNE_CONFIG = "${mkConfig}";
+        HOME = mkHome;
+      };
+      KeepAlive = true;
+      RunAtLoad = true;
+      StandardOutPath = "/tmp/vivienne-mk.log";
+      StandardErrorPath = "/tmp/vivienne-mk.err.log";
     };
   };
 }
