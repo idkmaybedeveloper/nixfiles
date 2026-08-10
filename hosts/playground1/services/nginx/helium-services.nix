0ae656e0a4ad08d84e7eeaa0e0509dd14816d34f
Lain Iwakura <lain@iwakura.page>
2026-08-11 00:02:34 +0300

    fix: meow

--- hosts/playground1/services/nginx/helium-services.nix
+++ hosts/playground1/services/nginx/helium-services.nix
@@ -71,9 +71,17 @@
     locations."= /connectivitycheck" = {
       return = "204";
     };
-    locations."/ext" = {
+    locations."= /ext" = {
       proxyPass = "http://helium-exts/";
-      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+      '';
+    };
+    locations."/ext/" = {
+      proxyPass = "http://helium-exts/";
       extraConfig = ''
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
