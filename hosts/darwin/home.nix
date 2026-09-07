afc0e4cafbd86844a008587bf2e9209e237cb745
Lain Iwakura <lain@iwakura.page>
2026-09-07 23:52:35 +0300

    feat: meow

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -11,8 +11,58 @@
     ./editor
   ];
 
+
+  #TODO: move to ./browser?
   programs.zen-browser = {
     enable = true;
     setAsDefaultBrowser = true;
+
+    policies = {
+      DisableAppUpdate = true;
+      DisableTelemetry = true;
+      DisablePocket = true;
+
+      ExtensionSettings = {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+      };
+    };
+
+    profiles.default = {
+      settings = {
+        "zen.workspaces.continue-where-left-off" = true;
+        "zen.urlbar.behavior" = "float";
+      };
+
+      search = {
+        force = true;
+        default = "kagi";
+        engines = {
+          kagi = {
+            name = "Kagi";
+            urls = [
+              {
+                template = "https://www.kagi.com/search?q={searchTerms}";
+              }
+            ];
+            definedAliases = ["@k"];
+          };
+        };
+      };
+
+      presets.catppuccin = {
+        enable = true;
+        flavor = "Mocha";
+        accent = "Mauve";
+      };
+    };
   };
 }
